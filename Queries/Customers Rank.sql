0a282313ace15479ede61635829486c313c1c39b
Sai Koushik Gandikota <saikoushik.gsk1@gmail.com>
2023-04-18 00:15:27 -0400

    Attaching Screen Shots

--- Queries/Customers Rank.sql
+++ Queries/Customers Rank.sql
@@ -1,6 +1,7 @@
 -- This Query gives the customers and their respective Number of Orders
 
-select c.customer_id,c.customer_name,count(co.order_id) as Number_of_orders
+select dense_rank() over (order by count(co.order_id) desc) as ID,
+c.customer_id,c.customer_name,count(co.order_id) as Number_of_orders
 from customers as c
 join customer_orders as co on c.customer_id=co.customer_id
 group by customer_id
